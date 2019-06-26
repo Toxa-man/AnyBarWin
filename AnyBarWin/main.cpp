a8d037f80ceb0cdd05a8baa37483cec73092cd67
Toxa <antonkviatofficial@gmail.com>
2019-06-26 11:54:45 +0300

    Added guard mutex to prevent multiple server instances running

--- AnyBarWin/main.cpp
+++ AnyBarWin/main.cpp
@@ -1,7 +1,7 @@
 #include "HeadlessAppWin.h"
 #include <thread>
 #include "Utils.h"
-#include "TrayIconHandlerWin.h"
+
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     _In_opt_ HINSTANCE hPrevInstance,
@@ -10,6 +10,21 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
+
+    CreateMutex(nullptr,
+        TRUE,
+        TEXT("InstanceGuardMutex"));
+    switch (GetLastError()) {
+    case ERROR_SUCCESS:
+        break;
+    case ERROR_ALREADY_EXISTS:
+        Utils::debug("Server instance is already running");
+        return -1;
+    default:
+        Utils::debug("Error occured while obtaining mutex");
+        Utils::debug("Last error: ", GetLastError());
+        break;
+    }
 
     HeadlessAppWin::setHInstance(hInstance);
     std::unique_ptr<HeadlessApp> app = std::make_unique<HeadlessAppWin>();
