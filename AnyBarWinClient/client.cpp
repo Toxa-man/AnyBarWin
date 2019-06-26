aa427ac8c8f44a2787ddd2d42ba4bea1144311bf
Toxa <antonkviatofficial@gmail.com>
2019-06-26 12:01:48 +0300

    Handled pipe exception

--- AnyBarWinClient/client.cpp
+++ AnyBarWinClient/client.cpp
@@ -12,15 +12,23 @@ int main(int argc, char* argv[])
         std::cout << "available images: transparent, white, red, green, blue, question" << std::endl;
         return -1;
     }
-    NamedPipeWin pipe("anyBar", NamedPipe::Write);
-    if (argc == 2) {
-        if (strcmp(argv[1], "quit") == 0) {
-            pipe.write(Protocol::packMessage(Protocol::Quit, ""));
-        } else {
-            pipe.write(Protocol::packMessage(Protocol::Icon, argv[1]));
-        }
+    try {
+        NamedPipeWin pipe("anyBar", NamedPipe::Write);
+        if (argc == 2) {
+            if (strcmp(argv[1], "quit") == 0) {
+                pipe.write(Protocol::packMessage(Protocol::Quit, ""));
+            }
+            else {
+                pipe.write(Protocol::packMessage(Protocol::Icon, argv[1]));
+            }
 
-    } else {
-        pipe.write(Protocol::packMessage(Protocol::Path, argv[2]));
+        }
+        else {
+            pipe.write(Protocol::packMessage(Protocol::Path, argv[2]));
+        }
     }
+    catch (const std::runtime_error& e) {
+        std::cout << "Error occured: " << e.what();
+    }
+
 }
